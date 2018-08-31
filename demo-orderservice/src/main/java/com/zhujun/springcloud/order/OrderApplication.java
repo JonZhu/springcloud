64c4ab74a579c94c4d5ce086b7e632b3c26b5369
zhujun <gzzhujun@bbdservice.com>
2018-08-31 15:29:38 +0800

    hystrix断路器

--- demo-orderservice/src/main/java/com/zhujun/springcloud/order/OrderApplication.java
+++ demo-orderservice/src/main/java/com/zhujun/springcloud/order/OrderApplication.java
@@ -3,7 +3,7 @@ package com.zhujun.springcloud.order;
 import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.cloud.netflix.turbine.EnableTurbine;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 /**
@@ -12,8 +12,8 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
  * @time 2018/8/28 14:57
  */
 @SpringBootApplication
-@EnableTurbine
 @EnableFeignClients
+@EnableCircuitBreaker
 public class OrderApplication {
     public static void main(String[] args) {
         new SpringApplicationBuilder(OrderApplication.class).web(WebApplicationType.SERVLET).run(args);
--- demo-orderservice/src/main/java/com/zhujun/springcloud/order/controller/OrderController.java
+++ demo-orderservice/src/main/java/com/zhujun/springcloud/order/controller/OrderController.java
@@ -1,5 +1,6 @@
 package com.zhujun.springcloud.order.controller;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import com.zhujun.springcloud.order.client.UserClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,8 +18,13 @@ public class OrderController {
     private UserClient userClient;
 
     @RequestMapping("/create")
+    @HystrixCommand(fallbackMethod = "createOrderFallback")
     public String createOrder(String good) {
         return "test createOrder, " + userClient.hello("abc");
+    }
+
+    public String createOrderFallback(String good) {
+        return "test createOrder createOrderFallback";
     }
 
 }
